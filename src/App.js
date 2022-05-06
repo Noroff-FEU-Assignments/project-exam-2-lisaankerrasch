c34d55924136fbab5257d8530bdec3c01eab296d
lisaankerrasch <lisa.ar.strom@gmail.com>
2022-05-06 15:59:11 +0200

    images

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/home/HomePage";
 import Nav from "./constants/Nav";
 import ContactPage from "./components/contact/ContactPage";
+import LoginPage from "./components/login/LoginPage";
 import AccommodationPage from "./components/accommodation/AccommodationPage";
 import AccommodationDetail from "./components/accommodation/AccommodationDetail";
 import EnquirePage from "./components/enquiries/EnquirePage";
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" exact element={<HomePage />}></Route>
         <Route path="/contact" exact element={<ContactPage />}></Route>
+        <Route path="/login" exact element={<LoginPage />}></Route>
         <Route
           path="/accommodation"
           exact

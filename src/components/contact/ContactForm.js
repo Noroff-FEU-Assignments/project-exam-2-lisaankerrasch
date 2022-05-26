ba1c796998d0150a7747ee68fc4be152895306b0
lisaankerrasch <71253664+lisaankerrasch@users.noreply.github.com>
2022-05-26 13:20:11 +0200

    Update ContactForm.js

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,39 +1,12 @@
 import { useForm } from "react-hook-form";
-// import { yupResolver } from "@hookform/resolvers/yup";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import * as yup from "yup";
-// import FormError from "../common/FormError";
-// import useAxios from "../hooks/useAxios";
 import { BASE_URL, MESSAGE } from "../../constants/api";
 import FormSuccess from "../common/FormSuccess";
 import ServerError from "../common/ServerError";
 import axios from "axios";
 
 const url = BASE_URL + MESSAGE;
-
-// const schema = yup.object().shape({
-//   first_name: yup
-//     .string()
-//     .required("Please enter your first name")
-//     .min(3, "The first name must be at least 3 characters"),
-// lastname: yup
-//   .string()
-//   .required("Please enter your last name")
-//   .min(4, "The first name must be at least 4 characters"),
-// phone: yup
-//   .number()
-//   .required("Please enter your phone number")
-//   .min(6, "This is not a valid number!"),
-// email: yup
-//   .string()
-//   .required("Please enter an email address")
-//   .email("Please enter a valid email address"),
-// message: yup
-//   .string()
-//   .required("Please enter your message")
-//   .min(10, "The message must be at least 10 characters"),
-// });
 
 export default function ContactForm() {
   const [submitting, setSubmitting] = useState(false);
@@ -42,7 +15,7 @@ export default function ContactForm() {
 
   const history = useNavigate();
 
-  const { register } = useForm();
+  const { register, handleSubmit } = useForm();
 
   async function onSubmit(data) {
     setSubmitting(true);
@@ -64,15 +37,8 @@ export default function ContactForm() {
     }
   }
 
-  function validateForm() {
-    if (document.contactForm.data.first_name.value === "") {
-      alert("Please provide your name!");
-      document.myForm.Name.focus();
-      return false;
-    }
-  }
   return (
-    <form name="contactForm" onSubmit={validateForm(onSubmit)}>
+    <form name="contactForm" onSubmit={handleSubmit(onSubmit)}>
       {success && <FormSuccess>Your message is sent!</FormSuccess>}
 
       {serverError && (
@@ -88,30 +54,22 @@ export default function ContactForm() {
               className="contact-input"
               {...register("data.first_name")}
             />
-            {/* {errors.first_name && (
-              <FormError>{errors.first_name.message}</FormError>
-            )}{" "} */}
             <input
               placeholder="Last name"
               className="contact-input"
               {...register("data.last_name")}
             />
-            {/* {errors.lastname && (
-              <FormError>{errors.lastname.message}</FormError>
-            )} */}
           </div>
           <input
             placeholder="Phone number"
             className="contact-input"
             {...register("data.phone")}
           />
-          {/* {errors.first_name && <FormError>{errors.phone.message}</FormError>} */}
           <input
             placeholder="Email"
             className="contact-input"
             {...register("data.email")}
           />
-          {/* {errors.first_name && <FormError>{errors.email.message}</FormError>}{" "} */}
         </div>
         <div className="contact__flex--2">
           <textarea
@@ -119,7 +77,6 @@ export default function ContactForm() {
             className="contact-textarea"
             {...register("data.message")}
           />
-          {/* {errors.first_name && <FormError>{errors.message.message}</FormError>}{" "} */}
         </div>
       </div>
       <div className="contact-button">
